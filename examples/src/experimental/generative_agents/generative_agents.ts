c4341da4264fca5afd484050407df9ccbde847be
Tobias Merkle <tgamerkle@gmail.com>
2023-07-28 16:17:13 -0400

    Revert "fix dialogue to update conversation with each new contribution"
    This reverts commit 7aed91a1f51a44ed4aef111b1d30d468db0b38a2.

--- examples/src/experimental/generative_agents/generative_agents.ts
+++ examples/src/experimental/generative_agents/generative_agents.ts
@@ -275,7 +275,7 @@ const Simulation = async () => {
     initialObservation: string
   ): Promise<void> => {
     // Starts the conversation bewteen two agents
-    let [, observation] = await agents[1].generateReaction(
+    const [, observation] = await agents[1].generateReaction(
       initialObservation
     );
     console.log("Initial reply:", observation);
@@ -287,7 +287,6 @@ const Simulation = async () => {
         const [stayInDialogue, agentObservation] =
           await agent.generateDialogueResponse(observation);
         console.log("Next reply:", agentObservation);
-        observation = agentObservation
         if (!stayInDialogue) {
           breakDialogue = true;
         }
